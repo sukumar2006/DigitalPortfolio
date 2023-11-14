95df43937433823b8ba31b254a86d2dad2814f00
sukumar2006 <150817757+sukumar2006@users.noreply.github.com>
2023-11-14 06:19:17 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,112 @@
+let words = document.querySelectorAll(".word");
+words.forEach((word)=>{
+    let letters = word.textContent.split("");
+    word.textContent="";
+    letters.forEach((letter)=>{
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });    
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length -1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = ()=>{
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter,i)=>{
+        setTimeout(() =>{
+            letter.className = "letter out";
+        },i * 80);
+    });
+    nextWord.style.opacity="1";
+    Array.from(nextWord.children).forEach((letter,i)=>{
+        letter.className = "letter behind";
+        setTimeout(()=>{
+            letter.className = "letter in";
+        },340 + i * 80);
+    });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText,3000)
+
+
+const circles = document.querySelectorAll('.circ');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for(let i = 0; i < dots ; i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i<percent ; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+
+})
+
+
+let menuLi = document.querySelectorAll('header div a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 50)
+})
+
+const observer =  new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scoll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+
+const scrollBottom = document.querySelectorAll(".scoll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+
+const scrollTop = document.querySelectorAll(".scoll-top");
+scrollTop.forEach((el)=>observer.observe(el));
+
+function showAlert(){
+    alert("The message was sent successfully!");
+}
+
+
+function toggleReadMore(){
+    var additionalContent = document.querySelector('.additional-content');
+    var readMoreBtn = document.getElementById('read-more-btn');
+    additionalContent.classList.toggle('hidden');
+    var buttonText = additionalContent.classList.contains('hidden') ? 'Read More' : 'Read Less';
+    readMoreBtn.innerText = buttonText;
+}
